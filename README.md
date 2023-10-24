315b9cc76aedd617c66b4720ddf85071b0efc318
craftidev <ivann.codev@gmail.com>
2023-10-24 18:33:51 +0200

    readme/gitignore

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # PHP 8 development environment with PHP-FPM, Nginx and MySQL, using Docker and Docker Compose
-
 MIT licensed from [dyarleniber GitHub repo](https://github.com/dyarleniber/docker-php/tree/master)
 
 # Features
 Exercises from ENI School on PHP
+- variables, references
+- arrays, matrixes, joining arrays, format in HTML table
+- modifying strings, regex
+- sorting
+- date/time operations
+- function
+- random
--- public/gcd.php
+++ public/gcd.php
@@ -1,23 +1,24 @@
 <?php
 
-function GCD($a, $b) {
-echo '<br />';
-echo "What is the Greatest Common Divisor of $a and $b?";
-while ($b !== 0) {
-echo '<br />';
-echo "$b is greater than 0";
-$temp = $b;
-$b = $a % $b;
-$whole_division = ($temp != 0) ? intval($a / $temp) : 0;
-echo '<br />';
-echo "After $a // $temp = $whole_division it remain: $b";
-$a = $temp;
-}
-echo '<br />';
-echo "We've found the GCD.";
-echo '<br />';
+function GCD($a, $b)
+{
+    echo '<br />';
+    echo "What is the Greatest Common Divisor of $a and $b?";
+    while ($b !== 0) {
+        echo '<br />';
+        echo "$b is greater than 0";
+        $temp = $b;
+        $b = $a % $b;
+        $whole_division = ($temp != 0) ? intval($a / $temp) : 0;
+        echo '<br />';
+        echo "After $a // $temp = $whole_division it remain: $b";
+        $a = $temp;
+    }
+    echo '<br />';
+    echo "We've found the GCD.";
+    echo '<br />';
 
-return $a;
+    return $a;
 }
 
 echo '<br />';
