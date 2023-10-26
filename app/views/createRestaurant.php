26644df7fd8de801467b4265ebd780f8e143459b
craftidev <ivann.codev@gmail.com>
2023-10-26 21:30:56 +0200

    store values if form is rejected

--- app/views/createRestaurant.php
+++ app/views/createRestaurant.php
@@ -1,39 +1,39 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $name = $_POST["name"];
-        $address = $_POST["address"];
-        $postalCode = $_POST["postalCode"];
-        $city = $_POST["city"];
-        $phone = $_POST["phone"];
-        $description = $_POST["description"];
+$name        = isset($_POST["name"])        ? $_POST["name"]        : "";
+$address     = isset($_POST["address"])     ? $_POST["address"]     : "";
+$postalCode  = isset($_POST["postalCode"])  ? $_POST["postalCode"]  : "";
+$city        = isset($_POST["city"])        ? $_POST["city"]        : "";
+$phone       = isset($_POST["phone"])       ? $_POST["phone"]       : "";
+$description = isset($_POST["description"]) ? $_POST["description"] : "";
 
-        echo "Name: " . $name . "<br/>";
-        echo "Address: " . $address . "<br/>";
-        echo "Postal code: " . $postalCode . "<br/>";
-        echo "City: " . $city . "<br/>";
-        echo "Phone: " . $phone . "<br/>";
-        echo "Description: " . $description . "<br/>";
-    }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo "Name: "        . $name .        "<br/>";
+    echo "Address: "     . $address .     "<br/>";
+    echo "Postal code: " . $postalCode .  "<br/>";
+    echo "City: "        . $city .        "<br/>";
+    echo "Phone: "       . $phone .       "<br/>";
+    echo "Description: " . $description . "<br/>";
+}
 ?>
 
 <form method="POST" action="index.php" class="flex-form">
-<label for="name">Name: </label>
-<input name="name" type="text" />
+    <label for="name">Name: </label>
+    <input name="name" type="text" value="<?= $name; ?>" required />
 
-<label for="address">Address: </label>
-<input name="address" type="text" />
+    <label for="address">Address: </label>
+    <input name="address" type="text" value="<?= $address; ?>" required />
 
-<label for="postalCode">Postal code: </label>
-<input name="postalCode" type="text" />
+    <label for="postalCode">Postal code: </label>
+    <input name="postalCode" type="number" value="<?= $postalCode; ?>" required />
 
-<label for="city">City: </label>
-<input name="city" type="text" />
+    <label for="city">City: </label>
+    <input name="city" type="text" value="<?= $city; ?>" required />
 
-<label for="phone">Phone: </label>
-<input name="phone" type="text" />
+    <label for="phone">Phone: </label>
+    <input name="phone" type="text" value="<?= $phone; ?>" required />
 
-<label for="description">Description: </label>
-<textarea name="description"></textarea>
+    <label for="description">Description: </label>
+    <textarea name="description"><?= $description; ?></textarea>
 
-<input type="submit" value="Submit" />
+    <input type="submit" value="Submit" />
 </form>
